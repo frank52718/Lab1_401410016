84299ea6b855684b46153a7c9ced6650594f8d0e
frank52718 <frank30945@gmail.com>
2015-09-24 11:41:36 +0800

    second upload

---
+++ part2.r
@@ -0,0 +1,12 @@
+print (X <-sum( ( 1:100)^2 ));
+print (Y <- seq(X));
+print (Y <- mean(X));
+
+?inspect
+?mean
+?seq
+
+?"stats"
+?swirl
+
+
---
+++ part3.r
@@ -0,0 +1,23 @@
+Wingcrd <- c(59,55,53.5,55,52.5,57.5,53,55)
+Tarsus <- c(22.3,19.7,20.8,20.3,20.8,21.5,20.6,21.5)
+Head <- c(31.2,30.4,30.6,30.3,30.3,30.8,32.5,NA)
+Wt <- c(9.5,13.8,14.8,15.2,15.5,15.6,15.6,15.7)
+print(mean(Wingcrd))
+print(mean(Tarsus))
+print(mean(Head,na.rm = TRUE))
+print(mean(Wt))
+
+print(median(Wingcrd))
+print(median(Tarsus))
+print(median(Head,na.rm = TRUE))
+print(median(Wt))
+
+print (min(Wingcrd))
+print (min(Tarsus))
+print (min(Head,na.rm = TRUE))
+print (min(Wt))
+
+print (max(Wingcrd))
+print (max(Tarsus))
+print (max(Head,na.rm = TRUE))
+print (max(Wt))
